4cca77903650a7513501ed712d1d3ad85cd37720
Schuyskaya <schujskaya@mail.ru>
2022-02-09 00:02:55 +0400

    add setInterval for .main-slider

--- src/js/script.js
+++ src/js/script.js
@@ -5,31 +5,43 @@ showSlides(slideIndex);
 
 function nextSlide() {
     showSlides(slideIndex += 1);
+    makeTimer();
 }
 
 function previousSlide() {
     showSlides(slideIndex -= 1);
+    makeTimer();
 }
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {    
+function showSlides(n) {
     let slides = document.getElementsByClassName("main-slider__item");
-    
+
     if (n > slides.length) {
-      slideIndex = 1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
-    
+
     for (let slide of slides) {
         slide.style.display = "none";
     }
-    
+
     slides[slideIndex - 1].style.display = "block";
+}
+
+let timer = 0;
+makeTimer();
+function makeTimer() {
+    clearInterval(timer);
+    timer = setInterval(function () {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 5000);
 }
 
 /* main-slider for index.html END */
