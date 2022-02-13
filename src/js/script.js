2fd28bbaa6c0f4400b4cd779dd683beffdfaf8da
Schuyskaya <schujskaya@mail.ru>
2022-02-13 09:03:07 +0400

    edit scripts

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let slides = document.getElementsByClassName("main-slider__item");
+  let slides = document.querySelectorAll(".main-slider__item");
 
   if (n > slides.length) {
     slideIndex = 1
@@ -48,92 +48,78 @@ function makeTimer() {
 
 /* main-slider for index.html END */
 
-/* subscribe_form START */
+/* subscribeForm START */
 
-window.addEventListener('load', () => {
+subscribeSend.addEventListener('click', sendMail);
 
-  let subscribe__button = document.getElementById('subscribe-send');
-  if (subscribe__button) {
-    subscribe__button.addEventListener('click', send_mail);
+let formRequired = subscribeForm.querySelectorAll('.required')
+for (let elem of formRequired) {
+  elem.addEventListener('input', valid_form__required)
+}
+
+function subscribeSendDisabled() {
+  subscribeSend.setAttribute('disabled', 'disabled')
+  subscribeSend.style.opacity = ''
+}
+
+function subscribeSendActive() {
+  subscribeSend.removeAttribute('disabled')
+  subscribeSend.style.opacity = '1'
+}
+
+function succesDisplay() {
+  let formSucces = document.querySelector('.subscribe-form__succes');
+  formSucces.style.display = 'block';
+  setTimeout(function () {
+    formSucces.style.display = 'none';
+  }, 5000);
+}
+
+function valid_form__required(e) {
+  let error = []
+  for (let elem of formRequired) {
+    if (elem.value.trim().length < 1) {
+      error.push(elem.id)
+    }
   }
 
-  let form__required = document.querySelectorAll('#subscribe_form .required')
-  for (let i = 0; i < form__required.length; i++) {
-    form__required[i].addEventListener('input', valid_form__required)
+  if (error.length == 0 && agree.checked) {
+    subscribeSendActive()
+  } else if (error.length > 0) {
+    subscribeSendDisabled();
   }
+}
 
-  function valid_form__required(e) {
-    let error = []
-    let inp = document.querySelectorAll('#subscribe_form .required')      
-    for (let i = 0; i < inp.length; i++) {
-      if (inp[i].value.trim().length < 1) {
-        error.push(inp[i].id)
+function sendMail(e) {
+
+  e.preventDefault();
+
+  let data = new FormData();
+  data.append('women', women.checked);
+  data.append('men', men.checked);
+  data.append('girls', girls.checked);
+  data.append('boys', boys.checked);
+  data.append('user_email', userEmail.value);
+  data.append('agree', agree.checked);
+
+  fetch('mail.php',
+    {
+      method: "POST",
+      body: data
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        return Promise.reject();
       }
-    }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(JSON.stringify(data));
+    })
+    .catch(() => console.log('Error'));
+  subscribeForm.reset();
+  subscribeSendDisabled();
+  succesDisplay();
+}
 
-    if (error.length == 0 && document.getElementById('agree').checked) {
-      subscribe__button.removeAttribute('disabled')
-      subscribe__button.style.opacity = '1'
-    } else if (error.length > 0) {
-      subscribe__button.setAttribute('disabled', 'disabled')
-      subscribe__button.style.opacity = ''
-    }
-  }
-
-  function send_mail(e) {
-    e.preventDefault();
-
-    let women = "";
-    if (document.getElementById('women').checked) {
-      women = document.getElementById('women').value;
-    }
-    let men = "";
-    if (document.getElementById('men').checked) {
-      men = document.getElementById('men').value;
-    }
-    let girls = "";
-    if (document.getElementById('girls').checked) {
-      girls = document.getElementById('girls').value;
-    }
-    let boys = "";
-    if (document.getElementById('boys').checked) {
-      boys = document.getElementById('boys').value;
-    }
-    let user_email = document.getElementById('user_email').value;
-    let agree = document.getElementById('agree').value;
-    document.getElementById('subscribe_form').reset();
-    subscribe__button.setAttribute('disabled', 'disabled')
-    subscribe__button.style.opacity = ''
-    document.querySelector('.subscribe-form__succes').style.display = 'block';
-    setTimeout(function () {
-      document.querySelector('.subscribe-form__succes').style.display = 'none';
-    }, 5000);
-
-    let data = new FormData();
-    data.append('women', women);
-    data.append('men', men);
-    data.append('girls', girls);
-    data.append('boys', boys);
-    data.append('user_email', user_email);
-    data.append('agree', agree);
-
-    fetch('mail.php',
-      {
-        method: "POST",
-        body: data
-      })
-      .then(response => {
-        if (response.status !== 200) {
-          return Promise.reject();
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(JSON.stringify(data));
-      })
-      .catch(() => console.log('Error'));
-  }
-
-})
-
-/* subscribe_form END */
+/* subscribeForm END */
