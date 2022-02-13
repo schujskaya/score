81438c3fc024a58da32c00e18db21e6023832d40
Schuyskaya <schujskaya@mail.ru>
2022-02-13 19:10:04 +0400

    add two form

--- mail.php
+++ mail.php
@@ -7,18 +7,24 @@ $men =  $data_obj->{'men'};
 $girls =  $data_obj->{'girls'}; 
 $boys =  $data_obj->{'boys'}; 
 $user_email =  $data_obj->{'user_email'}; 
+$user_email_grid =  $data_obj->{'user_email_grid'}; 
 $agree =  $data_obj->{'agree'}; 
 
 $to = "schujskaya@mail.ru";
 $subject = "Subscribe!";
 $message = '<html><body><h1>New subscriber</h1>';
 $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
-$message .= "<tr><td><strong>Women :</strong> </td><td>" . $women . "</td></tr>";
-$message .= "<tr><td><strong>Men :</strong> </td><td>" . $men . "</td></tr>";
-$message .= "<tr><td><strong>Girls :</strong> </td><td>" . $girls . "</td></tr>";
-$message .= "<tr><td><strong>Boys :</strong> </td><td>" . $boys . "</td></tr>";
-$message .= "<tr><td><strong>E-mail :</strong> </td><td>" . $user_email . "</td></tr>";
-$message .= "<tr><td><strong>Agree:</strong> </td><td>" . $agree . "</td></tr>";
+if($user_email){
+    $message .= "<tr><td><strong>Women :</strong> </td><td>" . $women . "</td></tr>";
+    $message .= "<tr><td><strong>Men :</strong> </td><td>" . $men . "</td></tr>";
+    $message .= "<tr><td><strong>Girls :</strong> </td><td>" . $girls . "</td></tr>";
+    $message .= "<tr><td><strong>Boys :</strong> </td><td>" . $boys . "</td></tr>";
+    $message .= "<tr><td><strong>E-mail :</strong> </td><td>" . $user_email . "</td></tr>";
+    $message .= "<tr><td><strong>Agree:</strong> </td><td>" . $agree . "</td></tr>";
+}
+if($user_email_grid){
+    $message .= "<tr><td><strong>E-mail :</strong> </td><td>" . $user_email_grid . "</td></tr>";   
+}
 $message .= "</table>";
 $message .= '<img src="http://schujsqp.beget.tech/src/images/subscribe2.png" />';
 $message .= "</body></html>";
@@ -26,6 +32,6 @@ $headers .= "Content-type: text/html; charset=$send_charset\r\n";
 
 mail($to, $subject, $message, $headers);
 
-echo $data_json;   
+echo $data_json;
 
 ?>
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 /* main-slider for index.html START */
 
+//const { data } = require("autoprefixer");
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -51,73 +53,89 @@ function makeTimer() {
 /* subscribeForm START */
 
 subscribeSend.addEventListener('click', sendMail);
+subscribeSendGrid.addEventListener('click', sendMail);
 
 let formInput = subscribeForm.querySelectorAll('input');
 for (let elem of formInput) {
   elem.oninput = function () {
-    if (validateEmail() && validateAgree()) {
-      subscribeSendActive();
+    if (validateEmail(subscribeForm) && validateAgree(subscribeForm)) {
+      buttonActive(subscribeSend);
     } else {
-      subscribeSendDisabled();
+      buttonDisabled(subscribeSend);
+    }
+  }
+}
+let formInputGrid = subscribeFormGrid.querySelectorAll('input');
+for (let elem of formInputGrid) {
+  elem.oninput = function () {
+    if (validateEmail(subscribeFormGrid)) {
+      buttonActive(subscribeSendGrid);
+    } else {
+      buttonDisabled(subscribeSendGrid);
     }
   }
 }
 
-function validateEmail() {
-  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  return reg.test(userEmail.value);
+function validateEmail(e) {
+  let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let emailValue = e.querySelector('.email');
+  return reg.test(emailValue.value);
 }
-function validateAgree() {
+
+function validateAgree(e) {
+  let agreeValue = e.querySelector('.agree');
   return agree.checked;
 }
 
-function subscribeSendDisabled() {
-  subscribeSend.setAttribute('disabled', 'disabled')
-  subscribeSend.style.opacity = ''
+function buttonDisabled(e) {
+  e.setAttribute('disabled', 'disabled');
+  e.style.opacity = '';
 }
 
-function subscribeSendActive() {
-  subscribeSend.removeAttribute('disabled')
-  subscribeSend.style.opacity = '1'
+function buttonActive(e) {
+  e.removeAttribute('disabled');
+  e.style.opacity = '1';
 }
 
-function succesDisplay() {
-  let formSucces = document.querySelector('.subscribe-form__succes');
+function succesDisplay(e) {
+  let formSucces = e.querySelector('.subscribe-form__succes');
   formSucces.style.display = 'block';
   setTimeout(function () {
     formSucces.style.display = 'none';
   }, 5000);
 }
 
-function sendData() {
-  let data = new FormData();
-
-  if (women.checked) {
-    data.append('women', women.value);
-  }
-  if (men.checked) {
-    data.append('men', men.value);
-  }
-  if (girls.checked) {
-    data.append('girls', girls.value);
-  }
-  if (boys.checked) {
-    data.append('boys', boys.value);
-  }
-  if (userEmail.value.length > 0) {
-    data.append('user_email', userEmail.value);
-  }  
-  if (agree.checked) {
-    data.append('agree', agree.value);
-  }
-
-  return data;  
-}
-
 function sendMail(e) {
-
   e.preventDefault();
-  data = sendData();
+  let buttonName = e.currentTarget;
+  let formName = buttonName.closest('form');
+  let data = new FormData();
+  switch (formName.id) {
+    case 'subscribeForm':
+      if (women.checked) {
+        data.append('women', women.value);
+      }
+      if (men.checked) {
+        data.append('men', men.value);
+      }
+      if (girls.checked) {
+        data.append('girls', girls.value);
+      }
+      if (boys.checked) {
+        data.append('boys', boys.value);
+      }
+      if (userEmail.value.length > 0) {
+        data.append('user_email', userEmail.value);
+      }
+      if (agree.checked) {
+        data.append('agree', agree.value);
+      }
+
+    case 'subscribeFormGrid':
+      if (userEmailGrid.value.length > 0) {
+        data.append('user_email_grid', userEmailGrid.value);
+      }
+  }
 
   fetch('mail.php',
     {
@@ -134,9 +152,8 @@ function sendMail(e) {
       console.log(JSON.stringify(data));
     })
     .catch(() => console.log('Error'));
-  subscribeForm.reset();
-  subscribeSendDisabled();
-  succesDisplay();
+  formName.reset();
+  succesDisplay(formName);
 }
 
 /* subscribeForm END */
